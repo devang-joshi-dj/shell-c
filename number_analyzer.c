1bb67c7797f8eb681aa80a70f7e9a516c45dabf4
Devang Joshi <devangjoshi11@gmail.com>
2026-05-23 19:32:46 +0530

    Added Text based UI functions in number_analyzer

--- number_analyzer.c
+++ number_analyzer.c
@@ -12,18 +12,37 @@
  *  - Prime number detection
  *  - Even/Odd detection
  *  - Magic number score
- * Execute by -> gcc binary_convertor.c -o bin/binary_convertor && ./bin/binary_convertor
+ * Execute by -> gcc number_analyzer.c -o bin/number_analyzer && ./bin/number_analyzer
  */
 #include <stdio.h> // for printf, scanf, getchar functions
 #include <stdlib.h> // for system function
+#include <string.h> // for strlen type
 
 int main();
 int accept_number(char *prompt);
 
+void drawLine(int width);
+void drawTitle(char *title, int width);
+void drawHeader(char *header, int width);
+void drawOpenBox(char *label, char *value, int width);
+void drawBoxBottom(int width);
+
 int main() {
+    const int FORMAT_WIDTH = 40;
+    int number = accept_number("Reveal thy number for analysis");
+
     system("clear"); // for clearing terminal in Linux/macOS
 
-    int number = accept_number("Reveal thy number for analysis");
+    drawTitle("NUMBER ANALYZER", FORMAT_WIDTH);
+
+    drawHeader("INPUT DETAILS", FORMAT_WIDTH);
+    drawOpenBox("Entered Number: ", "Num", FORMAT_WIDTH);
+    drawOpenBox("Data 2: ", "Value", FORMAT_WIDTH);
+    drawOpenBox("Data 3: ", "Value", FORMAT_WIDTH);
+    drawOpenBox("Data 4: ", "Value", FORMAT_WIDTH);
+    drawBoxBottom(FORMAT_WIDTH);
+
+    drawTitle("ANALYSIS COMPLETED SUCCESSFULLY", FORMAT_WIDTH);
 
     return 0;
 }
@@ -46,4 +65,44 @@ int accept_number(char *prompt) {
 	} while (value < 2);
 
 	return value;
+}
+
+void drawLine(int width) {
+    int actual_width = width-2;
+    printf("+"); // taking up 1 width
+    for (int i=0;i<actual_width;i++) printf("-"); // taking up 2 less than width
+    printf("+\n"); // taking up 1 width
+}
+void drawTitle(char *title, int width) {
+    int total_empty_space = width - strlen(title) - 2;
+    int one_side_spacing = total_empty_space / 2;
+    int other_side_spacing = total_empty_space - one_side_spacing;
+    drawLine(width);
+    printf("|"); // taking up 1 width
+    for (int i=0;i<one_side_spacing;i++) printf(" ");
+    printf("%s", title);
+    for (int i=0;i<other_side_spacing;i++) printf(" ");
+    printf("|\n"); // taking up 1 width
+    drawLine(width);
+    printf("\n");
+}
+void drawHeader(char *header, int width) {
+    int right_side_spacing = width - 2 - strlen(header);
+    drawLine(width);
+    printf("|"); // taking up 1 width
+    printf("%s", header);
+    for (int i=0;i<right_side_spacing;i++) printf(" ");
+    printf("|\n"); // taking up 1 width
+    drawLine(width);
+}
+void drawOpenBox(char *label, char *value, int width) {
+    int right_side_spacing = width - 2 - strlen(label) - strlen(value);
+    printf("|"); // taking up 1 width
+    printf("%s%s", label, value);
+    for (int i=0;i<right_side_spacing;i++) printf(" ");
+    printf("|\n"); // taking up 1 width
+}
+void drawBoxBottom(int width) {
+    drawLine(width);
+    printf("\n");
 }
